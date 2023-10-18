63a2cb85f1625de2c983a9be33fc2341207a3a61
QWESOUNT <96152899+qwesount@users.noreply.github.com>
2023-10-18 02:08:36 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -3,79 +3,54 @@ const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
 class Scratch3YourExtension {
-
-    constructor (runtime) {
-        // put any setup for your extension here
+    constructor(runtime) {
+        // Put any setup for your extension here
     }
 
     /**
      * Returns the metadata about your extension.
      */
-    getInfo () {
+    getInfo() {
         return {
-            // unique ID for your extension
+            // Unique ID for your extension
             id: 'yourScratchExtension',
 
-            // name that will be displayed in the Scratch UI
+            // Name that will be displayed in the Scratch UI
             name: 'Demo',
 
-            // colours to use for your extension blocks
+            // Colours to use for your extension blocks
             color1: '#000099',
             color2: '#660066',
 
-            // icons to display
+            // Icons to display
             blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
             menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
 
-            // your Scratch blocks
+            // Your Scratch blocks
             blocks: [
                 {
-                    // name of the function where your block code lives
+                    // MyFirstBlock definition
                     opcode: 'myFirstBlock',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
                     blockType: BlockType.REPORTER,
-
-                    // label to display on the block
                     text: 'Title for Book [BOOK_NUMBER]',
-
-                    // true if this block should end a stack
                     terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
+                    filter: [TargetType.SPRITE, TargetType.STAGE],
                     arguments: {
                         BOOK_NUMBER: {
                             defaultValue: 1718500564,
                             type: ArgumentType.NUMBER
-                        },
-                        {
-                            // function where your code logic lives
-                            opcode: 'mySecondBlock',
-                    
-                            // type of block
-                            blockType: BlockType.REPORTER,
-                    
-                            // label to display on the block
-                            text: 'Syllables in [MY_TEXT]',
-                    
-                            // arguments used in the block
-                            arguments: {
-                              MY_TEXT: {
-                                defaultValue: 'Hello World',
-                    
-                                // type/shape of the parameter
-                                type: ArgumentType.STRING
-                              }
+                        }
+                    }
+                },
+                {
+                    // MySecondBlock definition
+                    opcode: 'mySecondBlock',
+                    blockType: BlockType.REPORTER,
+                    text: 'Syllables in [MY_TEXT]',
+                    arguments: {
+                        MY_TEXT: {
+                            defaultValue: 'Hello World',
+                            type: ArgumentType.STRING
                         }
                     }
                 }
@@ -83,25 +58,30 @@ class Scratch3YourExtension {
         };
     }
 
-
     /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
+     * Implementation of the block with the opcode that matches this name.
+     * This will be called when the block is used.
      */
-    myFirstBlock ({ BOOK_NUMBER }) {
+    myFirstBlock({ BOOK_NUMBER }) {
         return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            else {
-              return { title: 'Unknown' };
-            }
-          })
-          .then((bookinfo) => {
-            return bookinfo.title;
-          });
-      }
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return { title: 'Unknown' };
+                }
+            })
+            .then((bookinfo) => {
+                return bookinfo.title;
+            });
+    }
 
+    mySecondBlock({ MY_TEXT }) {
+        // Implement syllable counting logic here
+        // Return the number of syllables in the provided text
+        // For example, you can use a library or function to count syllables
+        return countSyllables(MY_TEXT);
+    }
+}
 
 module.exports = Scratch3YourExtension;
